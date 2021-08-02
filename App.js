07fda95d3c26cc360d783e36048cd52cf6006448
João Colen <joaovictorcolen17@gmail.com>
2021-08-02 10:46:22 -0300

    First Commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Aplicativo João!</Text>
       <StatusBar style="auto" />
     </View>
   );
